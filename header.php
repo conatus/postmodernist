6804db2570318d581c3f1f37989ffe8b2c898893
alex@recordsonribs.com <alex@recordsonribs.com>
2010-10-02 23:26:10 +0100

    HTML Tidy

--- header.php
+++ header.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-	
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
-<head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<!--[if lt IE 8]>
+	<head profile="http://gmpg.org/xfn/11">
+		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=utf-8; ?>" />
+		<title>
+			<?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?>
+		</title>
+		<link rel="stylesheet" href="%3C?php%20bloginfo('stylesheet_url');%20?%3E" type="text/css" media="screen" /><!--[if lt IE 8]>
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/ie-fixes.css" type="text/css" media="screen" />
-	<![endif]-->	
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<script type="text/javascript" charset="utf-8">
-	//<![CDATA[
+	<![endif]-->
+		<link rel="pingback" href="%3C?php%20bloginfo('pingback_url');%20?%3E" />
+		<script type="text/javascript" charset="utf-8">
+//<![CDATA[
 		sfHover = function() {
 			var sfEls = document.getElementById("nav").getElementsByTagName("LI");
 			for (var i=0; i<sfEls.length; i++) {
@@ -24,27 +23,26 @@
 				}
 			}
 		}
-		if (window.attachEvent) window.attachEvent("onload", sfHover);		
-	//]]>
-	</script>
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-
-<div id="wrapper">	
-	<div id="header-wrapper">
-		<div id="nav">
-			<ul>
-				<?php wp_list_pages('title_li='); ?>
-			</ul>
+		if (window.attachEvent) window.attachEvent("onload", sfHover);      
+		//]]>
+		</script><?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<div id="wrapper">
+			<div id="header-wrapper">
+				<div id="nav">
+					<ul>
+						<?php wp_list_pages('title_li='); ?>
+					</ul>
+				</div>
+				<div id="header">
+					<h1>
+						<a href="%3C?php%20bloginfo('url');%20?%3E"><?php bloginfo('name'); ?></a>
+					</h1>
+				</div>
+			</div>
+			<hr />
+			<div id="content"></div>
 		</div>
-		
-		<div id="header">
-			<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-		</div>
-	</div>
-	
-	<hr />
-	
-	<div id="content">
+	</body>
+</html>
