33672af1898eea2fd55245a1c4360780a71ee633
alex@recordsonribs.com <alex@recordsonribs.com>
2010-10-03 21:28:28 +0100

    Removed all references to the post author as there is only me on this blog.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<div class="post-header">
 				<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<p>by <?php the_author(); ?> on <?php the_time('F j, Y') ?></p>
+				<p><?php the_time('F j, Y') ?></p>
 			</div>
 			
 			<?php the_post_thumbnail(array( 150, 150 ), array( 'class' => 'alignleft' )); ?>
